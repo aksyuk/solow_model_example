3029ceb27777c2a1efb6285da2cec6f597697a4c
aksyuk <s.a.aksuk@gmail.com>
2020-01-28 17:18:27 +0300

    Update CB_target.m

--- CB_target.m
+++ CB_target.m
@@ -1,5 +1,5 @@
-function err = CB_target(K, L, Y, x, w)
+function err = CB_target(K, L, Y, x, tau)
 % целевая функция - минимум суммы квадратов со скользящим окном
-K = K(w:end); L = L(w:end); Y = Y(w:end);
+K = K(tau:end); L = L(tau:end); Y = Y(tau:end);
 Y_fit = x(1) .* K .^ x(2) .* L .^ x(3);
 err = sum((Y_fit - Y) .^ 2);
